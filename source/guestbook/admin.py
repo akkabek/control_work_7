11d3b2ccc081680cf05eac38bda7d30a1acb442b
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-06-06 13:17:54 +0600

    Register model in admin

--- source/guestbook/admin.py
+++ source/guestbook/admin.py
@@ -1,1 +1,13 @@
 from django.contrib import admin
+from guestbook.models import GuestbookEntry
+
+
+
+class GuestbookEntryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'email', 'created_at']
+    list_filter = ['author','email']
+    search_fields = ['author', 'email']
+    fields = [ 'author','email', 'content', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+admin.site.register(GuestbookEntry, GuestbookEntryAdmin)
