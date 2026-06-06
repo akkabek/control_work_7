ea32e0c9b4dc4da0b486175b52e56434f2195b7f
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-06-06 16:43:08 +0600

    Shift app urls to guestbook from hello

--- source/guestbook/urls.py
+++ source/guestbook/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from guestbook.views import index_view
+from guestbook.views import index_view, entry_create_view
 
 urlpatterns = [
     path('', index_view, name='list'),
     path('entries/', index_view, name='list'),
-    path('entries/add', index_view, name='add'),
+    path('entries/add', entry_create_view, name='add'),
     ]
