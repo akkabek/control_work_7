d5bcb696ceb7f8cf92e1def8d5169ba79de1887e
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-06-06 13:44:53 +0600

    Add status to field

--- source/guestbook/admin.py
+++ source/guestbook/admin.py
@@ -7,7 +7,7 @@ class GuestbookEntryAdmin(admin.ModelAdmin):
     list_display = ['id', 'author', 'email', 'created_at']
     list_filter = ['author','email']
     search_fields = ['author', 'email']
-    fields = [ 'author','email', 'content', 'created_at', 'updated_at']
+    fields = [ 'author','email', 'content', 'status','created_at', 'updated_at']
     readonly_fields = ['created_at', 'updated_at']
 
 admin.site.register(GuestbookEntry, GuestbookEntryAdmin)
